c9e0cba2ccfe87d9e50f05321cdec53320dcd451
Zainya <77819437+Zainya@users.noreply.github.com>
2021-01-30 22:56:08 -0600

    Week2assignment

---
+++ index.js
@@ -0,0 +1,30 @@
+var y = 4  
+var x = 20
+
+console.log(x - y);
+
+var isHotOutside = true
+
+var isWeekday = false
+
+var hasMoneyInPocket = true
+
+
+
+var costOfMilk = 3.99;
+
+var moneyInWallet = 32;
+
+var thirstLevel = 7;
+
+var shouldBuyIcecream
+
+
+var willGoSwimming = true
+
+
+var willBuyMilk = true
+
+console.log(shouldBuyIcecream = isHotOutside && hasMoneyInPocket);
+
+console.log(willGoSwimming= isHotOutside && 3>=thirstLevel && moneyInWallet);
---
+++ loop.js
@@ -0,0 +1,30 @@
+var i = 0;
+
+while(i<=100) {
+
+    i+=2;
+
+    console.log(i);}
+
+    i = 3;
+while (i <= 100){
+    console.log(i);
+    i += 3;
+}
+
+{
+    for (let i=1; i<100; i++) 
+      if(i % 2 === 0) 
+        console.log(i);
+      }
+
+
+      {
+        for (let i=1; i<100; i++) 
+          if(i % 2 === 0) 
+            console.log(i);
+            while(i<5)("Hello");
+            console.log(i)
+          }
+          
+    
